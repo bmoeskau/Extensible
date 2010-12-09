82671b2a7197ba854671853cea975ea51938a799
Brian Moeskau <brian@moeskau.com>
2010-12-08 22:09:20 -0600

    Updated remote sample to make remote error testing more flexible

--- examples/calendar/remote.js
+++ examples/calendar/remote.js
@@ -14,7 +14,8 @@ Ext.onReady(function(){
         },
         listeners: {
             exception: function(proxy, type, action, o, res, arg){
-                //Ext.Msg.alert('Error', res.message);
+                var msg = res.message ? res.message : Ext.decode(res.responseText).message;
+                Ext.Msg.alert('Error', msg);
             }
         }
     });
@@ -39,18 +40,13 @@ Ext.onReady(function(){
         proxy: proxy,
         reader: reader,
         writer: writer,
-        
-        // force an error response to test handling. this param is only implemented
-        // in the back end code for this sample -- it's not default behavior.
-        //baseParams: { fail: true },
-        
         // the view will automatically set start / end date params for you. You can
         // also pass a valid config object as specified by Ext.data.Store.load()
         // and the start / end params will be appended to it.
         autoLoad: true
     });
     
-    new Ext.ensible.cal.CalendarPanel({
+    var cp = new Ext.ensible.cal.CalendarPanel({
         id: 'calendar-remote',
         eventStore: store,
         renderTo: 'remote',
@@ -68,4 +64,22 @@ Ext.onReady(function(){
     // likely not what you'll want. 
     // store.load({ ... });
     
+    
+    var errorCheckbox = Ext.get('forceError');
+     
+    var setRemoteErrorMode = function(){
+        if(errorCheckbox.dom.checked){
+            // force an error response to test handling of CUD (not R) actions. this param is 
+            // only implemented in the back end code for this sample -- it's not default behavior.
+            store.setBaseParam('fail', true);
+            cp.setTitle('Remote Calendar <span id="errTitle">(Currently in remote error mode)</span>');
+        }
+        else{
+            delete store.baseParams['fail'];
+            cp.setTitle('Remote Calendar');
+        }
+    };
+    
+    setRemoteErrorMode();
+    errorCheckbox.on('click', setRemoteErrorMode);
 });
