999b2a9da8ac11543804b3f882b49da5ebcdfa2f
Brian Moeskau <brian@moeskau.com>
2011-01-11 12:52:10 -0600

    Switched WeeView to extend MultiDayView to inherit the proper moveNext/Prev logic

--- src/calendar/views/WeekView.js
+++ src/calendar/views/WeekView.js
@@ -1,13 +1,13 @@
 /**
  * @class Ext.ensible.cal.WeekView
- * @extends Ext.ensible.cal.DayView
+ * @extends Ext.ensible.cal.MultiDayView
  * <p>Displays a calendar view by week. This class does not usually need to be used directly as you can
  * use a {@link Ext.ensible.cal.CalendarPanel CalendarPanel} to manage multiple calendar views at once including
  * the week view.</p>
  * @constructor
  * @param {Object} config The config object
  */
-Ext.ensible.cal.WeekView = Ext.extend(Ext.ensible.cal.DayView, {
+Ext.ensible.cal.WeekView = Ext.extend(Ext.ensible.cal.MultiDayView, {
     /**
      * @cfg {Number} dayCount
      * The number of days to display in the view (defaults to 7)
