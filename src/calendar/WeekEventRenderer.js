a0959f47a745ebe6fde299155c727848c4b7ffe1
Brian Moeskau <brian@moeskau.com>
2011-01-24 14:08:53 -0600

    Fixed bug when clicking in week view all day area, with no existing events, and always getting the firs tday's date in the add window.

--- src/calendar/WeekEventRenderer.js
+++ src/calendar/WeekEventRenderer.js
@@ -28,124 +28,109 @@ Ext.ensible.cal.WeekEventRenderer = function(){
                 dayCount = o.weekCount == 1 ? o.dayCount : 7;
             
             for(; w < weekCount; w++){
-                if(!grid[w] || grid[w].length == 0){
-                    // no events or span cells for the entire week
-                    if(weekCount == 1){
-                        row = getEventRow(o.id, w, 0);
-                        var cellCfg = {
-                            tag: 'td',
-                            cls: 'ext-cal-ev',
-                            id: o.id+'-empty-0-day-'+dt.format('Ymd'),
-                            html: '&#160;'
-                        }
-                        if(dayCount > 1){
-                            cellCfg.colspan = dayCount;
-                        }
-                        Ext.DomHelper.append(row, cellCfg);
-                    }
-                    dt = dt.add(Date.DAY, 7);
-                }else{
-                    var row, d = 0, wk = grid[w];
-                    var startOfWeek = dt.clone();
-                    var endOfWeek = startOfWeek.add(Date.DAY, dayCount).add(Date.MILLI, -1);
-                    
-                    for(; d < dayCount; d++){
-                        if(wk[d]){
-                            var ev = emptyCells = skipped = 0, 
-                                day = wk[d], ct = day.length, evt;
-                            
-                            for(; ev < ct; ev++){
-                                if(!day[ev]){
-                                    emptyCells++;
-                                    continue;
-                                }
-                                if(emptyCells > 0 && ev-emptyCells < max){
-                                    row = getEventRow(o.id, w, ev-emptyCells);
-                                    var cellCfg = {
-                                        tag: 'td',
-                                        cls: 'ext-cal-ev',
-                                        id: o.id+'-empty-'+ct+'-day-'+dt.format('Ymd')
-                                    }
-                                    if(emptyCells > 1 && max-ev > emptyCells){
-                                        cellCfg.rowspan = Math.min(emptyCells, max-ev);
-                                    }
-                                    Ext.DomHelper.append(row, cellCfg);
-                                    emptyCells = 0;
-                                }
-                                
-                                if(ev >= max){
-                                    skipped++;
-                                    continue;
-                                }
-                                evt = day[ev];
-                                
-                                if(!evt.isSpan || evt.isSpanStart){ //skip non-starting span cells
-                                    var item = evt.data || evt.event.data;
-                                    item._weekIndex = w;
-                                    item._renderAsAllDay = item[Ext.ensible.cal.EventMappings.IsAllDay.name] || evt.isSpanStart;
-                                    item.spanLeft = item[Ext.ensible.cal.EventMappings.StartDate.name].getTime() < startOfWeek.getTime();
-                                    item.spanRight = item[Ext.ensible.cal.EventMappings.EndDate.name].getTime() > endOfWeek.getTime();
-                                    item.spanCls = (item.spanLeft ? (item.spanRight ? 'ext-cal-ev-spanboth' : 
-                                        'ext-cal-ev-spanleft') : (item.spanRight ? 'ext-cal-ev-spanright' : ''));
-                                            
-                                    var row = getEventRow(o.id, w, ev),
-                                        cellCfg = {
-                                            tag: 'td',
-                                            cls: 'ext-cal-ev',
-                                            cn : eventTpl.apply(o.templateDataFn(item))
-                                        },
-                                        diff = Ext.ensible.Date.diffDays(dt, item[Ext.ensible.cal.EventMappings.EndDate.name]) + 1,
-                                        cspan = Math.min(diff, dayCount-d);
-                                        
-                                    if(cspan > 1){
-                                        cellCfg.colspan = cspan;
-                                    }
-                                    Ext.DomHelper.append(row, cellCfg);
-                                }
+                var row, d = 0, wk = grid[w];
+                var startOfWeek = dt.clone();
+                var endOfWeek = startOfWeek.add(Date.DAY, dayCount).add(Date.MILLI, -1);
+                
+                for(; d < dayCount; d++){
+                    if(wk && wk[d]){
+                        var ev = emptyCells = skipped = 0, 
+                            day = wk[d], ct = day.length, evt;
+                        
+                        for(; ev < ct; ev++){
+                            if(!day[ev]){
+                                emptyCells++;
+                                continue;
                             }
-                            if(ev > max){
-                                row = getEventRow(o.id, w, max);
-                                Ext.DomHelper.append(row, {
-                                    tag: 'td',
-                                    cls: 'ext-cal-ev-more',
-                                    id: 'ext-cal-ev-more-'+dt.format('Ymd'),
-                                    cn: {
-                                        tag: 'a',
-                                        html: String.format(o.moreText, skipped)
-                                    }
-                                });
-                            }
-                            if(ct < o.evtMaxCount[w]){
-                                row = getEventRow(o.id, w, ct);
-                                if(row){
-                                    var cellCfg = {
-                                        tag: 'td',
-                                        cls: 'ext-cal-ev',
-                                        id: o.id+'-empty-'+(ct+1)+'-day-'+dt.format('Ymd')
-                                    };
-                                    var rowspan = o.evtMaxCount[w] - ct;
-                                    if(rowspan > 1){
-                                        cellCfg.rowspan = rowspan;
-                                    }
-                                    Ext.DomHelper.append(row, cellCfg);
-                                }
-                            }
-                        }else{
-                            row = getEventRow(o.id, w, 0);
-                            if(row){
+                            if(emptyCells > 0 && ev-emptyCells < max){
+                                row = getEventRow(o.id, w, ev-emptyCells);
                                 var cellCfg = {
                                     tag: 'td',
                                     cls: 'ext-cal-ev',
-                                    id: o.id+'-empty-day-'+dt.format('Ymd')
-                                };
-                                if(o.evtMaxCount[w] > 1){
-                                    cellCfg.rowSpan = o.evtMaxCount[w];
+                                    html: '&#160;',
+                                    id: o.id+'-empty-'+ct+'-day-'+dt.format('Ymd')
+                                }
+                                if(emptyCells > 1 && max-ev > emptyCells){
+                                    cellCfg.rowspan = Math.min(emptyCells, max-ev);
+                                }
+                                Ext.DomHelper.append(row, cellCfg);
+                                emptyCells = 0;
+                            }
+                            
+                            if(ev >= max){
+                                skipped++;
+                                continue;
+                            }
+                            evt = day[ev];
+                            
+                            if(!evt.isSpan || evt.isSpanStart){ //skip non-starting span cells
+                                var item = evt.data || evt.event.data;
+                                item._weekIndex = w;
+                                item._renderAsAllDay = item[Ext.ensible.cal.EventMappings.IsAllDay.name] || evt.isSpanStart;
+                                item.spanLeft = item[Ext.ensible.cal.EventMappings.StartDate.name].getTime() < startOfWeek.getTime();
+                                item.spanRight = item[Ext.ensible.cal.EventMappings.EndDate.name].getTime() > endOfWeek.getTime();
+                                item.spanCls = (item.spanLeft ? (item.spanRight ? 'ext-cal-ev-spanboth' : 
+                                    'ext-cal-ev-spanleft') : (item.spanRight ? 'ext-cal-ev-spanright' : ''));
+                                        
+                                var row = getEventRow(o.id, w, ev),
+                                    cellCfg = {
+                                        tag: 'td',
+                                        cls: 'ext-cal-ev',
+                                        cn : eventTpl.apply(o.templateDataFn(item))
+                                    },
+                                    diff = Ext.ensible.Date.diffDays(dt, item[Ext.ensible.cal.EventMappings.EndDate.name]) + 1,
+                                    cspan = Math.min(diff, dayCount-d);
+                                    
+                                if(cspan > 1){
+                                    cellCfg.colspan = cspan;
                                 }
                                 Ext.DomHelper.append(row, cellCfg);
                             }
                         }
-                        dt = dt.add(Date.DAY, 1);
+                        if(ev > max){
+                            row = getEventRow(o.id, w, max);
+                            Ext.DomHelper.append(row, {
+                                tag: 'td',
+                                cls: 'ext-cal-ev-more',
+                                id: 'ext-cal-ev-more-'+dt.format('Ymd'),
+                                cn: {
+                                    tag: 'a',
+                                    html: String.format(o.moreText, skipped)
+                                }
+                            });
+                        }
+                        if(ct < o.evtMaxCount[w]){
+                            row = getEventRow(o.id, w, ct);
+                            if(row){
+                                var cellCfg = {
+                                    tag: 'td',
+                                    cls: 'ext-cal-ev',
+                                    //html: '&#160;',
+                                    id: o.id+'-empty-'+(ct+1)+'-day-'+dt.format('Ymd')
+                                };
+                                var rowspan = o.evtMaxCount[w] - ct;
+                                if(rowspan > 1){
+                                    cellCfg.rowspan = rowspan;
+                                }
+                                Ext.DomHelper.append(row, cellCfg);
+                            }
+                        }
+                    }else{
+                        row = getEventRow(o.id, w, 0);
+                        if(row){
+                            var cellCfg = {
+                                tag: 'td',
+                                cls: 'ext-cal-ev',
+                                html: '&#160;',
+                                id: o.id+'-empty-day-'+dt.format('Ymd')
+                            };
+                            if(o.evtMaxCount[w] > 1){
+                                cellCfg.rowSpan = o.evtMaxCount[w];
+                            }
+                            Ext.DomHelper.append(row, cellCfg);
+                        }
                     }
+                    dt = dt.add(Date.DAY, 1);
                 }
             }
         }
