5805396ee26b2bdff0fa806404b44fce54ce5dc0
Brian Moeskau <brian@moeskau.com>
2011-04-13 23:50:11 -0500

    Minor var reference fix in some examples

--- examples/calendar/custom-views.js
+++ examples/calendar/custom-views.js
@@ -1,6 +1,6 @@
 Ext.onReady(function(){
     
-    this.eventStore = new Ext.ensible.sample.MemoryEventStore({
+    var eventStore = new Ext.ensible.sample.MemoryEventStore({
         // defined in data/events.js
         data: Ext.ensible.sample.EventData
     });
