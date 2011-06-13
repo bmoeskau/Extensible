534754858f37e7f95ba7edd44dfeb1162bb6596b
Brian Moeskau <brian@moeskau.com>
2011-06-12 23:47:58 -0500

    Fix calendar StatusProxy for Ext 4

--- src/calendar/dd/StatusProxy.js
+++ src/calendar/dd/StatusProxy.js
@@ -20,61 +20,38 @@ Ext.define('Ext.ensible.cal.StatusProxy', {
      */
     addEventCls : 'ext-cal-dd-add',
     
-//    // private
-//    constructor: function(config){
-//        Ext.apply(this, config);
-//        this.id = this.id || Ext.id();
-//        this.proxy = Ext.createWidget('component', {
-//            floating: true,
-//            id: this.id,
-//            html: '<div class="' + Ext.baseCSSPrefix + 'dd-drop-icon"></div>' +
-//                  '<div class="' + Ext.baseCSSPrefix + 'dd-drag-ghost"><div class="ext-dd-msg"></div></div>',
-//            cls: Ext.baseCSSPrefix + 'dd-drag-proxy ' + this.dropNotAllowed,
-//            shadow: !config || config.shadow !== false,
-//            renderTo: document.body
-//        });
-//
-//        this.el = this.proxy.el;
-//        this.el.show();
-//        this.el.setVisibilityMode(Ext.core.Element.VISIBILITY);
-//        this.el.hide();
-//
-//        this.ghost = Ext.get(this.el.dom.childNodes[1]);
-//        this.dropStatus = this.dropNotAllowed;
-//    },
-//    constructor: function(config) {
-//        Ext.apply(this, config);
-//        this.id = this.id || Ext.id();
-//        this.el = new Ext.Layer({
-//            dh: {
-//                id: this.id, cls: 'ext-dd-drag-proxy x-dd-drag-proxy '+this.dropNotAllowed, cn: [
-//                    {cls: 'x-dd-drop-icon'},
-//                    {cls: 'ext-dd-ghost-ct', cn:[
-//                        {cls: 'x-dd-drag-ghost'},
-//                        {cls: 'ext-dd-msg'}
-//                    ]}
-//                ]
-//            }, 
-//            shadow: !config || config.shadow !== false
-//        });
-//        this.ghost = Ext.get(this.el.dom.childNodes[1].childNodes[0]);
-//        this.message = Ext.get(this.el.dom.childNodes[1].childNodes[1]);
-//        this.dropStatus = this.dropNotAllowed;
-//    },
+    // private
+    constructor: function(config){
+        Ext.apply(this, config);
+        this.id = this.id || Ext.id();
+        this.proxy = Ext.createWidget('component', {
+            floating: true,
+            id: this.id,
+            html: '<div class="' + Ext.baseCSSPrefix + 'dd-drop-icon"></div>' +
+                  '<div class="ext-dd-ghost-ct"><div class="' + Ext.baseCSSPrefix + 'dd-drag-ghost"></div>' +
+                  '<div class="ext-dd-msg"></div></div>',
+            cls: Ext.baseCSSPrefix + 'dd-drag-proxy ' + this.dropNotAllowed,
+            shadow: !config || config.shadow !== false,
+            renderTo: document.body
+        });
+
+        this.el = this.proxy.el;
+        this.el.show();
+        this.el.setVisibilityMode(Ext.core.Element.VISIBILITY);
+        this.el.hide();
+
+        this.ghost = Ext.get(this.el.dom.childNodes[1].childNodes[0]);
+        this.message = Ext.get(this.el.dom.childNodes[1].childNodes[1]);
+        this.dropStatus = this.dropNotAllowed;
+    },
 
     // inherit docs
     update : function(html){
-        if(typeof html == 'string'){
-            this.ghost.update(html);
-        }else{
-            this.ghost.update('');
-            html.style.margin = '0';
-            this.ghost.dom.appendChild(html);
-        }
+        this.callParent(arguments);
+        
         var el = this.ghost.dom.firstChild;
         if(el){
-            Ext.fly(el).setStyle('float', 'none').setHeight('auto');
-            Ext.getDom(el).id += '-ddproxy';
+            Ext.fly(el).setHeight('auto');
         }
     },
     
@@ -83,6 +60,6 @@ Ext.define('Ext.ensible.cal.StatusProxy', {
      * @param {String} msg The new status message
      */
     updateMsg : function(msg){
-        //this.message.update(msg);
+        this.message.update(msg);
     }
 });
