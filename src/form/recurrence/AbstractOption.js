f855488ffdd5be7eaafe0a873f666388d7b9870e
Brian Moeskau <brian@moeskau.com>
2012-01-08 01:38:50 -0600

    Got interval option implemented

---
+++ src/form/recurrence/AbstractOption.js
@@ -0,0 +1,26 @@
+Ext.define('Extensible.form.recurrence.AbstractOption', {
+    extend: 'Ext.form.FieldContainer',
+    
+    mixins: {
+        field: 'Ext.form.field.Field'
+    },
+    
+    layout: 'hbox',
+    defaults: {
+        margins: '0 5 0 0'
+    },
+    
+    initComponent: function() {
+        this.callParent();
+        this.initField();
+    },
+    
+    setStartDate: function(dt) {
+        this.startDate = dt;
+        
+        if (this.updateLabel) {
+            this.updateLabel();
+        }
+        return this;
+    },
+});
