d85e24d88b9e241b1a8a5de2c652b552bc6d6f37
Brian Moeskau <brian@moeskau.com>
2012-01-12 12:47:19 -0600

    Fix dd.StatusProxy for 4.1
    It was converted to a Component subclass from a plain class in 4.1, so some version-specific handling was needed.

--- src/calendar/dd/StatusProxy.js
+++ src/calendar/dd/StatusProxy.js
@@ -19,36 +19,73 @@ Ext.define('Extensible.calendar.dd.StatusProxy', {
      * The CSS class to apply to the status element when drop is not allowed (defaults to 'ext-cal-dd-add').
      */
     addEventCls : 'ext-cal-dd-add',
+
+    // Overridden to add a separate message element inside the ghost area.
+    // Applies only to Ext 4.1 and above, see notes in constructor
+    renderTpl: [
+        '<div class="' + Ext.baseCSSPrefix + 'dd-drop-icon"></div>',
+        '<div class="ext-dd-ghost-ct">',
+            '<div class="' + Ext.baseCSSPrefix + 'dd-drag-ghost"></div>',
+            '<div class="ext-dd-msg"></div>',
+        '</div>'
+    ],
+    
+    // private -- applies only to Ext 4.1 and above, see notes in constructor
+    renderSelectors: {
+        ghost: '.' + Ext.baseCSSPrefix + 'dd-drag-ghost',
+        message: '.ext-dd-msg'
+    },
     
     // private
-    constructor: function(config){
-        Ext.apply(this, config);
-        this.id = this.id || Ext.id();
-        this.proxy = Ext.createWidget('component', {
+    constructor: function(config) {
+        // In Ext 4.0.x StatusProxy was a plain class that did not inherit from Component,
+        // and all of its els were rendered inside the constructor. Unfortunately, because
+        // of this none of the standard Component lifecycle methods apply and so we are left
+        // with manually overriding the entire constructor function to inject our custom
+        // markup and set up our references.
+        //
+        // In 4.1 StatusProxy was switched to inherit from Component, so the renderTpl and 
+        // renderSelectors configs will kick in and generate the proper elements and refs
+        // automagically, and will be ignored by 4.0.x.
+        if (Ext.getVersion().isLessThan('4.1')) {
+            this.preComponentConstructor(config);
+        }
+        else {
+            this.callParent(arguments);
+        }
+    },
+    
+    // private -- applies only to Ext <4.1, see notes in constructor
+    preComponentConstructor: function(config) {
+        var me = this;
+        
+        Ext.apply(me, config);
+        
+        me.id = me.id || Ext.id();
+        me.proxy = Ext.createWidget('component', {
             floating: true,
-            id: this.id,
-            html: '<div class="' + Ext.baseCSSPrefix + 'dd-drop-icon"></div>' +
-                  '<div class="ext-dd-ghost-ct"><div class="' + Ext.baseCSSPrefix + 'dd-drag-ghost"></div>' +
-                  '<div class="ext-dd-msg"></div></div>',
-            cls: Ext.baseCSSPrefix + 'dd-drag-proxy ' + this.dropNotAllowed,
+            id: me.id,
+            html: me.renderTpl.join(''),
+            cls: Ext.baseCSSPrefix + 'dd-drag-proxy ' + me.dropNotAllowed,
             shadow: !config || config.shadow !== false,
             renderTo: document.body
         });
-
-        this.el = this.proxy.el;
-        this.el.show();
-        this.el.setVisibilityMode(Ext.core.Element.VISIBILITY);
-        this.el.hide();
-
-        this.ghost = Ext.get(this.el.dom.childNodes[1].childNodes[0]);
-        this.message = Ext.get(this.el.dom.childNodes[1].childNodes[1]);
-        this.dropStatus = this.dropNotAllowed;
+ 
+        me.el = me.proxy.el;
+        me.el.show();
+        me.el.setVisibilityMode(Ext.core.Element.VISIBILITY);
+        me.el.hide();
+ 
+        me.ghost = Ext.get(me.el.dom.childNodes[1].childNodes[0]);
+        me.message = Ext.get(me.el.dom.childNodes[1].childNodes[1]);
+        me.dropStatus = me.dropNotAllowed;
     },
-
+    
     // inherit docs
     update : function(html){
         this.callParent(arguments);
         
+        // If available, set the ghosted event el to autoHeight for visual consistency
         var el = this.ghost.dom.firstChild;
         if(el){
             Ext.fly(el).setHeight('auto');
