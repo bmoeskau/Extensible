2ea1a926b34436166f8e5eeca933195e44165244
Brian Moeskau <brian@moeskau.com>
2012-08-20 02:17:49 -0500

    Override extractData in Google CalendarReader
    Add mapping logic for event dates and all-day flag

--- src/calendar/google/CalendarReader.js
+++ src/calendar/google/CalendarReader.js
@@ -1,11 +1,15 @@
 Ext.define('Extensible.calendar.google.CalendarReader', {
     extend: 'Ext.data.reader.Json',
     alias : 'reader.extensible.googlecalendar',
+    
+    requires: [
+        'Extensible.calendar.google.EventMappings'
+    ],
 
     root: 'items',
     
     readRecords: function(data) {
-        var resultSet = this.callParent([data]),
+        var resultSet = this.callParent(arguments),
             headerData = {}
         
         // Append the header fields returned as part of the event list to the
@@ -19,5 +23,31 @@ Ext.define('Extensible.calendar.google.CalendarReader', {
         resultSet.headerData = headerData;
         
         return resultSet;
+    },
+    
+    extractData: function(root) {
+        var records = this.callParent(arguments),
+            EventMappings = Extensible.calendar.google.EventMappings,
+            data,
+            processed = [],
+            len = records.length,
+            i = 0;
+        
+        for (i = 0; i < len; i++) {
+            data = records[i].data;
+            
+            if (data[EventMappings.StartDateAllDay.name]) {
+                data[EventMappings.StartDate.name] = data[EventMappings.StartDateAllDay.name];
+                data[EventMappings.EndDate.name] = data[EventMappings.EndDateAllDay.name];
+                data[EventMappings.IsAllDay.name] = true;
+                
+                delete data[EventMappings.StartDateAllDay.name];
+                delete data[EventMappings.EndDateAllDay.name];
+            }
+            
+            processed.push(records[i]);
+        }
+        
+        return processed;
     }
 });
