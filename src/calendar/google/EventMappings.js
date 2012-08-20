4c1e085dc616e88d2010c116aaed6bfbeeba338c
Brian Moeskau <brian@moeskau.com>
2012-08-20 02:16:57 -0500

    Fix basic Google event mappings
    Added in temp *AllDay dates to handle the separate date mappings provided by Google. These are later stripped out conditionally in the reader

--- src/calendar/google/EventMappings.js
+++ src/calendar/google/EventMappings.js
@@ -5,6 +5,11 @@ function() {
     Ext.ns('Extensible.calendar.google');
     
     Extensible.calendar.google.EventMappings = {
+        EventId: {
+            name:    'EventId',
+            mapping: 'id',
+            type:    'string'
+        },
         Title: {
             name: 'Title',
             mapping: 'summary',
@@ -12,13 +17,39 @@ function() {
         },
         StartDate: {
             name: 'StartDate',
-            mapping: 'start.date',
+            mapping: 'start.datetime',
             type: 'date'
         },
         EndDate: {
             name: 'EndDate',
-            mapping: 'end.date',
+            mapping: 'end.datetime',
             type: 'date'
+        },
+        StartDateAllDay: {
+            name: 'StartDateAllDay',
+            mapping: 'start.date',
+            type: 'date',
+            dateFormat: 'Y-m-d'
+        },
+        EndDateAllDay: {
+            name: 'EndDateAllDay',
+            mapping: 'end.date',
+            type: 'date',
+            dateFormat: 'Y-m-d'
+        },
+        StartTimeZone: {
+            name: 'StartTimeZone',
+            mapping: 'start.timeZone',
+            type: 'string'
+        },
+        EndTimeZone: {
+            name: 'EndTimeZone',
+            mapping: 'end.timeZone',
+            type: 'string'
+        },
+        IsAllDay: {
+            name:    'IsAllDay',
+            type:    'boolean'
         }
     };
     
