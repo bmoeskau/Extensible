4988ba799b959209d6f96521ee669ebd35fc5f0a
Brian Moeskau <brian@moeskau.com>
2012-09-25 23:35:41 -0500

    Fix minor overlapping event layout bug

--- src/calendar/view/AbstractCalendar.js
+++ src/calendar/view/AbstractCalendar.js
@@ -873,7 +873,7 @@ viewConfig: {
             len = day.length;
 
         for (; i < len; i++) {
-            if (day[i] === null) {
+            if (!day[i]) {
                 return i;
             }
         }
