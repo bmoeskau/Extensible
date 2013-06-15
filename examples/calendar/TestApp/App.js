1eb4c8367d891a038ffc2b9c258baed39418f82a
Gabriel Sidler <sidler@teamup.com>
2013-06-15 19:42:03 +0200

    First version of simple event list with support for grouping by month and week.

--- examples/calendar/TestApp/App.js
+++ examples/calendar/TestApp/App.js
@@ -105,6 +105,7 @@ Ext.define('Extensible.example.calendar.TestApp.App', {
                         //viewStartHour: 6,
                         //viewEndHour: 18,
                         //minEventDisplayMinutes: 15
+                        startDay: 0,
                         showTime: false
                     },
                     
@@ -125,6 +126,13 @@ Ext.define('Extensible.example.calendar.TestApp.App', {
                         dateRangeDefault: '3months'
                     },
 
+                    listViewCfg: {
+                        linkDatesToDayView: true,
+                        dateRangeDefault: '3months',
+                        simpleList: true,
+                        groupBy: 'month'
+                    },
+
                     // Some optional CalendarPanel configs to experiment with:
                     //readOnly: true,
                     //showDayView: false,
@@ -133,6 +141,7 @@ Ext.define('Extensible.example.calendar.TestApp.App', {
                     //showMultiWeekView: false,
                     //showMonthView: false,
                     showAgendaView: true,
+                    showListView: true,
                     //showNavBar: false,
                     //showTodayText: false,
                     //showTime: false,
