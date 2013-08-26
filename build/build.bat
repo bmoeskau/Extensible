3dcb77bf93ed0440363442597349e96569dd9dde
Brian Moeskau <brian@moeskau.com>
2013-08-26 11:28:30 -0500

    Tab -> space

--- build/build.bat
+++ build/build.bat
@@ -56,10 +56,7 @@ IF "%1" == "-d" (
         --title="Extensible Docs" ^
         --footer="<a href='http://ext.ensible.com/'>Ext.ensible.com</a>" ^
         --warnings=-all ^
-        --welcome="%EXTENSIBLE_ROOT%/welcome.html" ^
-        --examples="%EXTENSIBLE_ROOT%/examples.json" ^
-        --examples-base-url="../examples" ^
-        --categories="%EXTENSIBLE_ROOT%/categories.json" ^
+        --exclude="%EXTENSIBLE_ROOT%/src/calendar/dd/CalendarScrollManager.js" ^
         --ignore-html=locale,debug
 )
 
