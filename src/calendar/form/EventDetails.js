12638ac96542aff7e409312da82c48c15f2a6d66
Brian Moeskau <brian@moeskau.com>
2016-02-04 05:13:05 -0200

    Fix null error on form reset with recurrence active

--- src/calendar/form/EventDetails.js
+++ src/calendar/form/EventDetails.js
@@ -1,13 +1,13 @@
 /**
  * A custom form used for detailed editing of events.
- * 
+ *
  * This is pretty much a standard form that is simply pre-configured for the options needed by the
  * calendar components. It is also configured to automatically bind records of type
  * {@link Extensible.calendar.data.EventModel EventModel} to and from the form.
- * 
+ *
  * This form also provides custom events specific to the calendar so that other calendar components can be easily
  * notified when an event has been edited via this component.
- * 
+ *
  * The default configs are as follows:
  *		labelWidth: 65,
  *		labelWidthRightCol: 65,
@@ -38,7 +38,7 @@
 Ext.define('Extensible.calendar.form.EventDetails', {
     extend: 'Ext.form.Panel',
     alias: 'widget.extensible.eventeditform',
-    
+
     requires: [
         'Extensible.form.field.DateRange',
         'Extensible.calendar.form.field.ReminderCombo',
@@ -48,7 +48,7 @@ Ext.define('Extensible.calendar.form.EventDetails', {
         'Ext.layout.container.Column',
         'Extensible.form.recurrence.RangeEditWindow'
     ],
-    
+
     labelWidth: 65,
     labelWidthRightCol: 65,
     colWidthLeft: '.95',
@@ -87,7 +87,7 @@ Ext.define('Extensible.calendar.form.EventDetails', {
      * if the server supports it.
      */
     recurrence: false,
-    
+
     /**
      * @cfg {Boolean} allowDefaultAdd
      * @since 1.6.0
@@ -104,11 +104,11 @@ Ext.define('Extensible.calendar.form.EventDetails', {
      */
     allowDefaultAdd: true,
 
-	//private properties    
+	//private properties
     layout: 'column',
-    
+
     initComponent: function() {
-        
+
         this.addEvents({
             /**
              * @event eventadd
@@ -143,7 +143,7 @@ Ext.define('Extensible.calendar.form.EventDetails', {
              */
             eventcancel: true
         });
-                
+
         this.titleField = Ext.create('Ext.form.field.Text', {
             fieldLabel: this.titleLabelText,
             name: Extensible.calendar.data.EventMappings.Title.name,
@@ -180,14 +180,14 @@ Ext.define('Extensible.calendar.form.EventDetails', {
             name: Extensible.calendar.data.EventMappings.Url.name,
             anchor: this.fieldAnchor
         });
-        
+
         // var leftFields = [this.titleField, this.dateRangeField, this.reminderField],
             // rightFields = [this.notesField, this.locationField, this.urlField];
-            
+
         var rightFields = [],
             leftFields  = [this.titleField, this.dateRangeField, this.reminderField,
                            this.notesField, this.locationField, this.urlField];
-            
+
         if(this.recurrence) {
             this.recurrenceField = Ext.create('Extensible.form.recurrence.Fieldset', {
                 recurrenceOptions: this.recurrence,
@@ -201,7 +201,7 @@ Ext.define('Extensible.calendar.form.EventDetails', {
             });
             leftFields.splice(2, 0, this.recurrenceField);
         }
-        
+
         if(this.calendarStore) {
             this.calendarField = Ext.create('Extensible.calendar.form.field.CalendarCombo', {
                 store: this.calendarStore,
@@ -211,11 +211,11 @@ Ext.define('Extensible.calendar.form.EventDetails', {
             });
             leftFields.splice(2, 0, this.calendarField);
         }
-        
+
         // Now that all fields are in one column by default, make sure we use
         // the largest configured label width for all fields:
         var labelWidth = Math.max(this.labelWidthRightCol, this.labelWidth);
-        
+
         this.items = [{
             id: this.id+'-left-col',
             columnWidth: this.colWidthLeft,
@@ -235,7 +235,7 @@ Ext.define('Extensible.calendar.form.EventDetails', {
             border: false,
             items: rightFields
         }];
-        
+
         this.fbar = [{
             text:this.saveButtonText, scope: this, handler: this.onSave
         },{
@@ -243,58 +243,58 @@ Ext.define('Extensible.calendar.form.EventDetails', {
         },{
             text:this.cancelButtonText, scope: this, handler: this.onCancel
         }];
-        
+
         this.addCls('ext-evt-edit-form');
-        
+
         Ext.apply(this.initialConfig, {
             trackResetOnLoad: true
         });
-        
+
         this.callParent(arguments);
     },
-    
+
     onDateChange: function(dateRangeField, val) {
-        if (this.recurrenceField) {
+        if (val && val[0] && this.recurrenceField) {
             this.recurrenceField.setStartDate(val[0]);
         }
     },
-    
+
     onRecurrenceStartChange: function(recurrenceFieldset, startDate, oldDate) {
         this.dateRangeField.setValue(startDate);
     },
-    
+
     /**
-     * @protected 
+     * @protected
      */
     loadRecord: function(rec) {
         var me = this,
             EventMappings = Extensible.calendar.data.EventMappings;
-        
+
         me.form.reset().loadRecord.apply(me.form, arguments);
         me.activeRecord = rec;
         me.dateRangeField.setValue(rec.data);
-        
+
         if (me.recurrenceField) {
             var recurrenceStart = rec.get(EventMappings.RSeriesStartDate.name) ||
                 rec.get(EventMappings.StartDate.name);
-            
+
             // Prevent a loop since the two start date fields sync on change
             me.recurrenceField.suspendEvents();
             me.recurrenceField.setStartDate(recurrenceStart);
             me.recurrenceField.setValue(rec.get(EventMappings.RRule.name));
             me.recurrenceField.resumeEvents();
-            
+
             if (!rec.data[EventMappings.RInstanceStartDate.name]) {
                 // If the record is new we have to set the instance start date explicitly to match the
                 // field's default so that it does not show up later as dirty if it is not edited:
                 rec.data[EventMappings.RInstanceStartDate.name] = rec.getStartDate();
             }
         }
-        
+
         if (me.calendarField) {
             me.calendarField.setValue(rec.data[EventMappings.CalendarId.name]);
         }
-        
+
         if (rec.phantom) {
             me.setTitle(me.titleTextAdd);
             me.down('#' + me.id + '-del-btn').hide();
@@ -303,16 +303,16 @@ Ext.define('Extensible.calendar.form.EventDetails', {
             me.setTitle(me.titleTextEdit);
             me.down('#' + me.id + '-del-btn').show();
         }
-        
+
         // Using setValue() results in dirty fields, so we reset the field state
         // after loading the form so that the current values are the "original" values
         me.form.getFields().each(function(item) {
             item.resetOriginalValue();
         });
-        
+
         me.titleField.focus();
     },
-    
+
     updateRecord: function(record) {
         var fields = record.fields,
             values = this.getForm().getValues(),
@@ -326,63 +326,63 @@ Ext.define('Extensible.calendar.form.EventDetails', {
                 obj[name] = values[name];
             }
         });
-        
+
         var dates = this.dateRangeField.getValue(),
             allday = obj[EventMappings.IsAllDay.name] = dates[2],
             // Clear times for all day events so that they are stored consistently
             startDate = allday ? Extensible.Date.clearTime(dates[0]) : dates[0],
             endDate = allday ? Extensible.Date.clearTime(dates[1]) : dates[1],
             singleDayDurationConfig = { days: 1 };
-        
+
         // The full length of a day based on the minimum event time resolution:
         singleDayDurationConfig[Extensible.calendar.data.EventModel.resolution] = -1;
-        
+
         obj[EventMappings.StartDate.name] = startDate;
-        
+
         // If the event is all day, calculate the end date as midnight of the day after the end
         // date minus 1 unit based on the EventModel resolution, e.g. 23:59:00 on the end date
         obj[EventMappings.EndDate.name] = allday ?
             Extensible.Date.add(endDate, singleDayDurationConfig) : endDate;
-        
+
         if (EventMappings.Duration) {
             obj[EventMappings.Duration.name] = Extensible.Date.diff(startDate, obj[EventMappings.EndDate.name],
                 Extensible.calendar.data.EventModel.resolution);
         }
-        
+
         record.set(obj);
-        
+
         return record.dirty || (record.phantom && this.allowDefaultAdd);
     },
-    
+
     onCancel: function() {
         this.cleanup(true);
         this.fireEvent('eventcancel', this, this.activeRecord);
     },
-    
+
     cleanup: function(hide) {
         if (this.activeRecord) {
             this.activeRecord.reject();
         }
         delete this.activeRecord;
-        
+
         if (this.form.isDirty()) {
             this.form.reset();
         }
     },
-    
+
     onSave: function() {
         var me = this,
             originalHasRecurrence = me.activeRecord.isRecurring();
-        
+
         if (!me.form.isValid() && !me.allowDefaultAdd) {
             return;
         }
-        
+
         if (!me.updateRecord(me.activeRecord)) {
             me.onCancel();
             return;
         }
-        
+
         if (me.activeRecord.phantom) {
             me.fireEvent('eventadd', me, me.activeRecord);
         }
@@ -398,7 +398,7 @@ Ext.define('Extensible.calendar.form.EventDetails', {
             }
         }
     },
-    
+
     onRecurrenceUpdate: function() {
         this.rangeEditWin = this.rangeEditWin || Ext.WindowMgr.get('ext-cal-rangeeditwin');
         if (!this.rangeEditWin) {
@@ -409,10 +409,10 @@ Ext.define('Extensible.calendar.form.EventDetails', {
             scope: this
         });
     },
-    
+
     onRecurrenceEditModeSelected: function(editMode) {
         var me = this;
-        
+
         if (editMode) {
             me.activeRecord.data[Extensible.calendar.data.EventMappings.REditMode.name] = editMode;
             me.fireEvent('eventupdate', me, me.activeRecord, me.animateTarget);
